f01ba0af2757ef6d29141cd2eb1569ca9ea3ced3
Lucas Bicudo <Lucas.Bicudo@techsolcom.local>
2017-02-01 16:19:18 -0500

    Bonus Task 3 - Completed

--- module5-solution/src/public/sign-up/sign-up.controller.js
+++ module5-solution/src/public/sign-up/sign-up.controller.js
@@ -11,14 +11,31 @@
     signUpCtrl.registerOK = false;
 
     signUpCtrl.register = function () {
-      MenuService.getItem(signUpCtrl.registerData.favoriteDish).then(function (result) {
-        signUpCtrl.invalidFavoriteDish = false;
-        signUpCtrl.registerData.favoriteDishItem = result;
-        UserService.loginUser(signUpCtrl.registerData);
-        signUpCtrl.registerOK = true;
-      }, function (error) {
-        signUpCtrl.invalidFavoriteDish = true;
-      });
+      UserService.loginUser(signUpCtrl.registerData);
+      signUpCtrl.registerOK = true;
+
+      // MenuService.getItem(signUpCtrl.registerData.favoriteDish).then(function (result) {
+      //   signUpCtrl.invalidFavoriteDish = false;
+      //   signUpCtrl.registerData.favoriteDishItem = result;
+      //   UserService.loginUser(signUpCtrl.registerData);
+      //   signUpCtrl.registerOK = true;
+      // }, function (error) {
+      //   signUpCtrl.invalidFavoriteDish = true;
+      // });
+    }
+
+    signUpCtrl.invalidFavoriteDishCheck = function () {
+      if (signUpCtrl.registerData) {
+        MenuService.getItem(signUpCtrl.registerData.favoriteDish).then(function (result) {
+          signUpCtrl.invalidFavoriteDish = false;
+          signUpCtrl.registerData.favoriteDishItem = result;
+          return false;
+        }, function (error) {
+          signUpCtrl.invalidFavoriteDish = true;
+          return true;
+        });
+      }
+      return true;
     }
 
   }
